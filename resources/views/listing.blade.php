4aa9a4fff25bc8f68397b018f35d03135e2e13db
MohiZzine <farid.mohieddine3@gmail.com>
2023-06-14 11:20:40 +0100

    first commit

---
+++ resources/views/listing.blade.php
@@ -0,0 +1,15 @@
+<h2>
+  {{$listing['title']}}
+</h2>
+<p>
+  {{$listing['description']}}
+</p>
+
+{{-- @if(count($listings) == 0)
+    <p>No listings found</p>
+@endif
+
+@foreach($listings as $listing)
+    <h2>{{$listing['title']}}</h2>
+    <p>{{$listing['description']}}</p>
+@endforeach --}}
---
+++ resources/views/listings.blade.php
@@ -0,0 +1,27 @@
+<h1>{{$heading}}</h1>
+@unless(count($listings) == 0)
+
+@foreach($listings as $listing)
+<h2>
+    <a href="/listings/{{$listing['id']}}">
+        {{$listing['title']}}
+    </a>
+</h2>
+<p>
+    {{$listing['description']}}
+</p>
+@endforeach
+
+@else
+    <p>No listings found</p>
+@endunless
+
+
+{{-- @if(count($listings) == 0)
+    <p>No listings found</p>
+@endif
+
+@foreach($listings as $listing)
+    <h2>{{$listing['title']}}</h2>
+    <p>{{$listing['description']}}</p>
+@endforeach --}}
